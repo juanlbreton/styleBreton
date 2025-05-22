1981a17f6ba6c5e36dc8c4e40b0f8eda369b3eee
Juan L. Bretón, PMP <jlbreton@exatec.tec.mx>
2025-05-22 08:49:30 -0600

    Base font size changed to accommodate for recent changes en ggsave()

--- R/theme_Breton.R
+++ R/theme_Breton.R
@@ -8,17 +8,24 @@
 #' @export
 theme_breton <- function() {
   ggplot2::theme(
-    text = element_text(family = "Encode Sans Condensed"),
+    text = element_text(
+      family = "Encode Sans Condensed",
+      size = 10
+    ),
     panel.background = element_rect(fill = "#FFFFFF"),
     panel.grid = element_line(color = "grey95"),
     axis.line = element_line(color = "darkgrey"),
-    plot.title = element_text(face = "bold",
-                              size = rel(1.55)),
+    plot.title = element_text(
+      face = "bold",
+      size = rel(1.55)
+    ),
     plot.title.position = "plot",
-    plot.caption = ggtext::element_markdown(color = "darkgrey",
-                                            hjust = 0,
-                                            lineheight = 1.25,
-                                            size = rel(0.85)),
+    plot.caption = ggtext::element_markdown(
+      color = "darkgrey",
+      hjust = 0,
+      lineheight = 1.25,
+      size = rel(0.85)
+    ),
     plot.caption.position = "plot"
   )
 }
